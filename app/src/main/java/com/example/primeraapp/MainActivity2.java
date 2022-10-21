f57ab179fdff7415cd8ef3baf9ea068d2297a438
matiasr12 <maias.reinoso@gmail.com>
2022-10-21 09:35:55 -0300

    v 1.2

--- app/src/main/java/com/example/primeraapp/MainActivity2.java
+++ app/src/main/java/com/example/primeraapp/MainActivity2.java
@@ -20,5 +20,12 @@ public class MainActivity2 extends AppCompatActivity {
         myRef.setValue("Hello, World!");
 
     }
+    public void insertar(){
+// Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+
+    }
 }
 
