ec62ad972b395c04cf31094371c18bb7cfb9f3e4
matiasr12 <maias.reinoso@gmail.com>
2022-10-21 09:32:28 -0300

    v 1.2

--- app/src/main/java/com/example/primeraapp/MainActivity.kt
+++ app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.primeraapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
- class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        Toast.makeText(,MainActivity "hola", Toast.LENGTH_SHORT).show()
+        insertar():
+
     }
+     public void insertar(){
+
+         // Write a message to the database
+         // Write a message to the database
+         val database = FirebaseDatabase.getInstance()
+         val myRef = database.getReference("message")
+
+         myRef.setValue("Hello, World!")
+     }
 }
